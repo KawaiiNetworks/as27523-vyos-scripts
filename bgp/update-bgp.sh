f1db44fead61d4805378d4373a42118b6aebe171
canoziia <canoziia@qq.com>
2024-06-18 11:10:28 +0800

    add metric

--- bgp/update-bgp.sh
+++ bgp/update-bgp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -O /tmp/set-bgp.core.mci.atlas.projectk.org.sh https://github.com/KawaiiNetworks/vyos-scripts/releases/download/nightly/set-bgp.core.mci.atlas.projectk.org.sh
-vbash /tmp/set-bgp.core.mci.atlas.projectk.org.sh
-rm /tmp/set-bgp.core.mci.atlas.projectk.org.sh
+wget -O /config/myapp/set-bgp.core.mci.atlas.projectk.org.sh https://github.com/KawaiiNetworks/vyos-scripts/releases/download/nightly/set-bgp.core.mci.atlas.projectk.org.sh
+vbash /config/myapp/set-bgp.core.mci.atlas.projectk.org.sh | tee /config/myapp/set-bgp.log
+rm /config/myapp/set-bgp.core.mci.atlas.projectk.org.sh
