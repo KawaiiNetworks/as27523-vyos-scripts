ac026e8125fae6c4f30762d56913f57f1d89c990
Canoziia <canoziia@projectk.org>
2026-06-16 03:57:58 +0800

    fix container memory

--- tests/expected/configure.core.test.example.sh
+++ tests/expected/configure.core.test.example.sh
@@ -58,6 +58,7 @@ set service snmp v3 view default oid 1
 delete container name bird
 set container name bird image 'kawaiinetworks/bird:2'
 set container name bird allow-host-networks
+set container name bird memory 0
 set container name bird capability 'net-admin'
 set container name bird restart 'always'
 
